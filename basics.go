4f2326b8b76f49e739148997d4d07e3f18a20558
rishikesh <rishikeshkk123@gmail.com>
2026-04-19 17:14:21 +0530

    feat: add functionality to input and display age with conditional output in basics.go

--- basics.go
+++ basics.go
@@ -15,4 +15,17 @@ func main() {
 	fmt.Println("enter your name")
 	fmt.Scanln(&name2)
 	fmt.Println(name2)
+	var jink int
+	fmt.Println("enter the age of jink")
+	fmt.Scanln(&jink)
+	fmt.Println(jink)
+	if jink > 29 {
+		fmt.Println("jink is old")
+	} else {
+		fmt.Println("jink is young")
+	}
+
+	for i := 0; i < jink; i++ {
+		fmt.Println(i)
+	}
 }
