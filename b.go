9aa44118b111a9738bda361b4b98f058e8386843
rishikesh <rishikeshkk123@gmail.com>
2026-04-19 12:53:30 +0530

    new learning

---
+++ b.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("fuck i")
+	name := "rishikesh"
+	age := 3
+	fmt.Println(name)
+	fmt.Println(age)
+}
--- investment.go
+++ investment.go
@@ -1,13 +1,17 @@
-/*
 package main
-import  "fmt"
-import "math"
-func main(){
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
 	var investmentAmount = 1000
 	var annualInterestRate = 5.5
 	var years = 10
-	var futureValue = float64 (investmentAmount) * (1 + annualInterestRate/100)
-	fmt.Printf("Future Value of the Investment: %.2f\n", futureValue)
 
+	var futureValue = float64(investmentAmount) *
+		math.Pow(1+annualInterestRate/100, float64(years))
+
+	fmt.Printf("Future Value of the Investment: %.2f\n", futureValue)
 }
-*/
