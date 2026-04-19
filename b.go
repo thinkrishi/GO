faf0e4cc7f913b9d658182e31ff3ebacae5500df
rishikesh <rishikeshkk123@gmail.com>
2026-04-19 12:57:50 +0530

    fix: correct output message and improve age eligibility check

--- b.go
+++ b.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("fuck i")
+	fmt.Println("first go program")
 	name := "rishikesh"
 	age := 3
 	fmt.Println(name)
 	fmt.Println(age)
+
+	if age > 18 {
+		fmt.Println("You are eligible to vote.")
+	} else {
+		fmt.Println("minor")
+	}
+  
 }
